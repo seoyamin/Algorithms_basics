9d3a20e1ced338fcd0a0ec32b8694b0cdc9e19de
seoyamin <seoyamin@ewhain.net>
2022-06-06 15:36:10 +0900

    [2022.06.06] 버블정렬

---
+++ 01.Sort_Algorithms/01.버블정렬.cpp
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+void bubbleSort(int arr[], int len) {
+    int temp = 0; 
+    
+    for(int i=0 ; i<len ; i++) {
+        for(int j=0 ; j<len-i ; j++) {
+            if(arr[j] > arr[j+1]) {
+                swap(arr[j], arr[j+1], temp);
+                // temp = arr[j];
+                // arr[j] = arr[j+1];
+                // arr[j+1] = temp;
+            }
+        }
+    }
+    
+    for(int i=0 ; i<len ; i++) {
+        printf("%d ", arr[i]);
+    }
+}
+
+
+int main() {
+    int arr[] = {40, 10, 50, 90, 20, 80, 30, 60};
+    int len = sizeof(arr) / sizeof(int);
+    
+    bubbleSort(arr, len);
+    
+    return 0;
+}
